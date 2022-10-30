9fbc624dce6798c70c3554b2f6d7ef02b0d2a310
saiaunghlyanhtet <71708201+saiaunghlyanhtet@users.noreply.github.com>
2022-10-30 09:04:01 +0630

    RecommendationRunner and Recommender files added.

---
+++ src/mainsrc/Recommender.java
@@ -0,0 +1,8 @@
+package mainsrc;
+import java.util.*;
+
+public interface Recommender {
+	public ArrayList<String> getItemsToRate ();
+	
+	public void printRecommendationsFor (String webRaterID);
+}
